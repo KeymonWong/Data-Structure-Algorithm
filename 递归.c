71f8407a14fe9cff3fc6e22938fae87314dbcb86
huangqimeng <huangqimeng@xiaoban.mobi>
2019-07-11 23:18:23 +0800

    汉诺塔

--- 递归.c
+++ 递归.c
@@ -24,13 +24,35 @@ void print()
     }
 }
 
+/** 汉诺塔 */
+// 将 n 个盘子从 x 借助 y 移动到 z
+void move(int n, char x, char y, char z)
+{
+    if (n == 1)
+    {
+        printf("%c --> %c\n", x, z);
+    }
+    else
+    {
+        move(n - 1, x, z, y); // 将 n-1 个盘子从 x 借助 z 移动到 y 上
+        printf("%c --> %c\n", x, z); // 将第 n 个盘子从 x 移动到 z 上
+        move(n - 1, y, x, z); //将 n-1 个盘子从 y 借助 x 移动到 z 上
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     int res;
     res = Fib(10);
     printf("%d\n", res);
 
-    print();
+    int n;
+    printf("请输入汉诺塔的层数：");
+    scanf("%d", &n);
+    printf("移动的步骤如下：\n");
+    move(n, 'X', 'Y', 'Z');
+
+    // print();
     
     return 0;
 }
